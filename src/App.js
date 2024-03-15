5fbae7b2dbdeb33192a9d90a6c271c9d3961a8af
Pranav Kale <pranavkale021998@gmail.com>
2024-03-16 01:01:22 +0530

    Added Boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./screens/LandingPage";
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Router onUpdate={() => window.scrollTo(0, 0)}>
+          <Routes>
+            <Route path="/" exact element={<LandingPage/>} />
+            <Route path="/signup" exact element={<SignUp/>} />
+            <Route path="/signin" exact element={<SignIn/>} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/screens/SignIn/SignIn.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const SignIn = () => {
+  return (
+    <>
+      <h1>This is Sign in Page</h1>
+    </>
+  );
+};
+
+export default SignIn;
