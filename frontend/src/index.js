2322a5f8a475b0f25deeff719e6f121d7d96bdae
gautam-bhagat <gautambhagat.business@gmail.com>
2024-03-16 11:17:58 +0530

    following folder structure

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <GoogleOAuthProvider clientId="545731931393-3rk79lqagn370picn06a2oq8juq82cv0.apps.googleusercontent.com">
+    <App />
+  </GoogleOAuthProvider>
+
+);
