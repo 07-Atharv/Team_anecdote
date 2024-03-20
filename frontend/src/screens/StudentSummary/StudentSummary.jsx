3625cd2f4a6e125a7ebb070e9fcaa6fc089d42bd
Pranav Kale <pranavkale021998@gmail.com>
2024-03-20 17:29:46 +0530

    Updated

--- frontend/src/screens/StudentSummary/StudentSummary.jsx
+++ frontend/src/screens/StudentSummary/StudentSummary.jsx
@@ -5,7 +5,9 @@ import { useLocation } from 'react-router-dom';
 
 function StudentSummary() {
 
+    const [score, setScore] = useState(0);
     const { state } = useLocation();
+    const [res, setRes ] = useState();
 
     console.log()
     const [exams, setExams] = useState([])
@@ -23,9 +25,11 @@ function StudentSummary() {
         })
 
         res = await res.json()
-        console.log(res)
+        console.log( res );
+
         if (res.success === 1) {
-            setExams(exams)
+            setExams(res.exams)
+            setRes( res );
         }
 
     }
@@ -40,7 +44,7 @@ function StudentSummary() {
                 <img src={scoreCraftLogo} alt="ScoreCraft Logo" className="h-20" />
             </div>
 
-            <h1 className="text-6xl my-4 font-serif	text-center mt-20">Subject Scores</h1>
+            <h1 className="text-6xl my-4 font-serif	text-center mt-20">Your Subject Scores</h1>
 
             <div className="max-w-screen-xl mx-auto">
                 <div className="grid grid-cols-3 gap-4">
@@ -48,9 +52,9 @@ function StudentSummary() {
                     {/* Upper Row */}
                     {
                         exams.map((exam, i) => {
-                            return (<div key={i} className="bg-white rounded-lg shadow-md p-8 mx-auto my-10 w-5/6">
-                                <h2 className="text-xl font-semibold mb-4">Mathematics</h2>
-                                <p className="text-gray-700">Score: 85%</p>
+                            return (<div key={i} className="bg-white rounded-lg shadow-md p-8 mx-auto my-10 w-[400px]">
+                                <h2 className="text-3xl font-bold mb-4">{ res.examDets[i].title }</h2>
+                                <p className="text-gray-800 text-xl ">Score: {(( exam.score * 100 ) / exam.answers.length).toFixed(2) }%</p>
                             </div>)
                         })
                     }
